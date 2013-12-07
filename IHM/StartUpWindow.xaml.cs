b5824ec05620d832d6116357f71ce7a0ec357052
clemgaut <clemgaut@gmail.com>
2013-12-07 17:31:11 +0100

    Ajout d'une fenêtre de lancement

---
+++ IHM/StartUpWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IHM
+{
+    /// <summary>
+    /// Interaction logic for StartUpWindow.xaml
+    /// </summary>
+    public partial class StartUpWindow : Window
+    {
+        public StartUpWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win = new MainWindow();
+            win.Show();
+            this.Close();
+        }
+    }
+}
