c4db6e552aaab0658555244476ecea0e7bcac526
clemgaut <clemgaut@gmail.com>
2014-01-06 17:19:03 +0100

    First unit test

--- UnitTestProject/TestMapCreation.cs
+++ UnitTestProject/TestMapCreation.cs
@@ -28,8 +28,67 @@ namespace UnitTestProject
 
             Assert.AreNotEqual(demoBuilder.getGame().getMap().getMap(), null);
 
+            Assert.AreEqual(demoBuilder.getGame().getMap().Height, 5);
+            Assert.AreEqual(demoBuilder.getGame().getMap().Width, 5);
+
             foreach (Box b in demoBuilder.getGame().getMap().getMap())
             {
+                Assert.AreNotEqual(b, null);
+                Assert.IsInstanceOfType(b, typeof(Box));
+            }
+        }
+
+        [TestMethod]
+        public void TestSmallMapCreation() {
+            SmallMap smallMap = new SmallMap();
+
+            Assert.AreEqual(smallMap.Height, 10);
+            Assert.AreEqual(smallMap.Width, 10);
+
+            foreach (Box b in smallMap.getMap()) {
+                Assert.AreNotEqual(b, null);
+                Assert.IsInstanceOfType(b, typeof(Box));
+            }
+        }
+
+        [TestMethod]
+        public void TestSmallGameBuilderMapCreation() {
+            SmallGameBuilder smallBuilder = new SmallGameBuilder(ENation.GAUL, ENation.NAIN);
+
+            Assert.AreNotEqual(smallBuilder.getGame().getMap().getMap(), null);
+
+            Assert.AreEqual(smallBuilder.getGame().getMap().Height, 10);
+            Assert.AreEqual(smallBuilder.getGame().getMap().Width, 10);
+
+            foreach (Box b in smallBuilder.getGame().getMap().getMap()) {
+                Assert.AreNotEqual(b, null);
+                Assert.IsInstanceOfType(b, typeof(Box));
+            }
+        }
+
+        [TestMethod]
+        public void TestNormalMapCreation() {
+            NormalMap normalMap = new NormalMap();
+
+            Assert.AreEqual(normalMap.Height, 15);
+            Assert.AreEqual(normalMap.Width, 15);
+
+            foreach (Box b in normalMap.getMap()) {
+                Assert.AreNotEqual(b, null);
+                Assert.IsInstanceOfType(b, typeof(Box));
+            }
+        }
+
+        [TestMethod]
+        public void TestNormalGameBuilderMapCreation() {
+            NormalGameBuilder normalBuilder = new NormalGameBuilder(ENation.GAUL, ENation.VIKING);
+
+            Assert.AreNotEqual(normalBuilder.getGame().getMap().getMap(), null);
+
+            Assert.AreEqual(normalBuilder.getGame().getMap().Height, 15);
+            Assert.AreEqual(normalBuilder.getGame().getMap().Width, 15);
+
+            foreach (Box b in normalBuilder.getGame().getMap().getMap()) {
                 Assert.AreNotEqual(b, null);
                 Assert.IsInstanceOfType(b, typeof(Box));
             }
---
+++ UnitTestProject/TestUnitActions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTestProject {
+    [TestClass]
+    public class TestUnitActions {
+        private int[] lmap = 
+        { (int)EBoxType.DESERT, (int)EBoxType.FOREST, (int)EBoxType.FOREST, (int)EBoxType.MOUTAIN, (int)EBoxType.MOUTAIN,
+        (int)EBoxType.SEA, (int)EBoxType.LOWLAND, (int)EBoxType.FOREST, (int)EBoxType.MOUTAIN, (int)EBoxType.MOUTAIN,
+        (int)EBoxType.SEA, (int)EBoxType.LOWLAND, (int)EBoxType.LOWLAND, (int)EBoxType.DESERT, (int)EBoxType.SEA,
+        (int)EBoxType.LOWLAND, (int)EBoxType.SEA, (int)EBoxType.FOREST, (int)EBoxType.DESERT, (int)EBoxType.SEA,
+        (int)EBoxType.MOUTAIN, (int)EBoxType.LOWLAND, (int)EBoxType.FOREST, (int)EBoxType.DESERT, (int)EBoxType.MOUTAIN};
+
+        private Game getDemoGame(ENation nation1, int line1, int column1, ENation nation2, int line2, int column2) {
+            DemoGameBuilder demoBuilder = new DemoGameBuilder(nation1, nation2);
+            Game game = demoBuilder.getGame();
+            DemoMap map = new DemoMap();
+            map.convertIntListToMap(new List<int>(lmap));
+            game.setMap(map);
+            game.getPlayer1().getNation().moveToNullPosition();
+            game.getPlayer2().getNation().moveToNullPosition();
+            game.getPlayer1().getNation().setInitBox(line1, column1, map);
+            game.getPlayer2().getNation().setInitBox(line2, column2, map);
+
+            return game;
+        }
+        
+        [TestMethod]
+        public void TestPointsNain() {
+            Game game = getDemoGame(ENation.NAIN, 0, 0, ENation.GAUL, 4, 2);
+            Map map = game.getMap();
+
+            game.start();
+
+            Assert.AreEqual(game.getActivePlayer().getNation().getUnit(0).GetType(), typeof(NainUnit));
+            Assert.IsTrue(game.getActivePlayer().getNation().getUnit(0).move(0, 1, map));
+
+            game.nextStep();
+
+            Assert.AreEqual(game.getActivePlayer().getNation().getUnit(0).GetType(), typeof(GaulUnit));
+
+            Assert.AreEqual(game.getUnactivePlayer().getPoints(), 5);
+
+            game.nextStep();
+            Assert.AreEqual(game.getActivePlayer().getNation().getUnit(0).GetType(), typeof(NainUnit));
+
+            Assert.IsTrue(game.getActivePlayer().getNation().getUnit(0).move(1, 1, map));
+
+            game.nextStep();
+
+            Assert.AreEqual(game.getActivePlayer().getNation().getUnit(0).GetType(), typeof(GaulUnit));
+
+            Assert.AreEqual(game.getUnactivePlayer().getPoints(), 8);
+        }
+    }
+}
