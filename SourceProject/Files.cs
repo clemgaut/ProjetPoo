30cd65dc64e2f59a48ddd49ec34fd354051194a4
MatthieuBertin <matthieu.bertin@outlook.com>
2014-01-06 14:30:57 +0100

    Set up build conf x86

---
+++ SourceProject/Files.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+class Files {
+
+    private const string path = @"Saves";
+
+    public static List<String> getSaves() {
+
+        if(!Directory.Exists(path))
+            return null;
+
+        List<String> files = new List<String>(Directory.GetFiles(path));
+        files.RemoveAll(f => Path.GetExtension(f) != ".small");
+        files.ForEach(f => Path.GetFileNameWithoutExtension(f));
+
+        return files;
+    }
+
+    // load or save the file
+    public static void saveHandle(string fileName, bool load) {
+
+        // If save directory does not exist.
+        if(!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileAccess fa = (load) ? FileAccess.Read : FileAccess.Write;
+        string fullPath = path + Path.DirectorySeparatorChar + fileName + ".small";
+
+        Stream fs = new FileStream(fullPath, FileMode.OpenOrCreate, fa);
+
+        try {
+            if(load)
+                Game.instance = ((Game)formatter.Deserialize(fs));
+            else
+                formatter.Serialize(fs, Game.instance);
+        } catch(SerializationException e) {
+            Console.WriteLine("Serialization failed. Reason: " + e.Message);
+        } finally {
+            fs.Close();
+        }
+
+    }
+
+}
+
