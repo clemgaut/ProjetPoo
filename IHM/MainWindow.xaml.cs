9b92cf807325e3b013e76084d216f877b65f22b8
clemgaut <clemgaut@gmail.com>
2014-01-08 20:00:43 +0100

    IHM problems

--- IHM/MainWindow.xaml.cs
+++ IHM/MainWindow.xaml.cs
@@ -21,13 +21,13 @@ namespace IHM {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-        Map map;
-        Game game;
-        GameBuilder builder;
-        Rectangle selectedVisual;
-        ImageBrushFactory imageBrushFactory = new ImageBrushFactory();
+        Map _map;
+        Game _game;
+        GameBuilder _builder;
+        Rectangle _selectedVisual;
+        ImageBrushFactory _imageBrushFactory = new ImageBrushFactory();
         StackPanel _selectedUnit;
-        Wrapper.WrapperAlgo wrap = new Wrapper.WrapperAlgo();
+        Wrapper.WrapperAlgo _wrap = new Wrapper.WrapperAlgo();
 
         StartUpWindow start;
 
@@ -39,15 +39,15 @@ namespace IHM {
 
             switch(mapType) {
                 case EGameType.DEMO:
-                    builder = new DemoGameBuilder(nation1, nation2);
+                    _builder = new DemoGameBuilder(nation1, nation2);
                     break;
 
                 case EGameType.SMALL:
-                    builder = new SmallGameBuilder(nation1, nation2);
+                    _builder = new SmallGameBuilder(nation1, nation2);
                     break;
 
                 case EGameType.NORMAL:
-                    builder = new NormalGameBuilder(nation1, nation2);
+                    _builder = new NormalGameBuilder(nation1, nation2);
                     break;
 
                 default:
@@ -57,17 +57,17 @@ namespace IHM {
             }
             MapLabel.Content = "Type de carte : " + mapType.ToString().ToLower();
 
-            game = builder.getGame();
+            _game = _builder.getGame();
 
-            game.getPlayer1().setName(nameP1);
-            game.getPlayer2().setName(nameP2);
+            _game.getPlayer1().setName(nameP1);
+            _game.getPlayer2().setName(nameP2);
 
             Nation1Label.Content = nameP1 + " [" + nation1.ToString() + "]";
             Nation2Label.Content = nameP2 + " [" + nation2.ToString() + "]";
 
             _selectedUnit = null;
 
-            game.start();
+            _game.start();
         }
 
         public MainWindow(StartUpWindow win) {
@@ -78,16 +78,16 @@ namespace IHM {
             start = win;
             start.inGame = true;
 
-            game = Game.instance;
+            _game = Game.instance;
 
-            MapLabel.Content = "Type de carte : " + game.getMap().GetType().ToString().ToLower();
+            MapLabel.Content = "Type de carte : " + _game.getMap().GetType().ToString().ToLower();
 
-            Nation1Label.Content = game.getPlayer1().getName() + " [" + game.getPlayer1().getNation().nationType + "]";
-            Nation2Label.Content = game.getPlayer2().getName() + " [" + game.getPlayer2().getNation().nationType + "]";
+            Nation1Label.Content = _game.getPlayer1().getName() + " [" + _game.getPlayer1().getNation().nationType + "]";
+            Nation2Label.Content = _game.getPlayer2().getName() + " [" + _game.getPlayer2().getNation().nationType + "]";
 
             _selectedUnit = null;
 
-            game.start();
+            _game.start();
         }
 
         /*
@@ -99,10 +99,10 @@ namespace IHM {
 
             setDefaultOpacity();
 
-            game.nextStep();
+            _game.nextStep();
             updateForStep();
-            if (game.checkEndfOfGame()) {
-                winner = game.getWinner();
+            if (_game.checkEndfOfGame()) {
+                winner = _game.getWinner();
 
                 if (winner == null)
                     MessageBox.Show(this, "Match nul !", "Fin du jeu", MessageBoxButton.OK, MessageBoxImage.None);
@@ -116,16 +116,16 @@ namespace IHM {
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e) {
-            map = game.getMap();
+            _map = _game.getMap();
 
-            for(int i = 0; i < map.Width; i++) {
+            for(int i = 0; i < _map.Width; i++) {
                 mapGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(50, GridUnitType.Pixel) });
                 mapGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(50, GridUnitType.Pixel) });
             }
 
-            for(int l = 0; l < map.Height; l++) {
-                for(int c = 0; c < map.Width; c++) {
-                    var tile = map.getBox(l, c);
+            for(int l = 0; l < _map.Height; l++) {
+                for(int c = 0; c < _map.Width; c++) {
+                    var tile = _map.getBox(l, c);
                     var rect = createTile(l, c, tile);
                     mapGrid.Children.Add(rect);
                 }
@@ -140,15 +140,15 @@ namespace IHM {
         private Grid createTile(int l, int c, Box tile) {
             var rectangle = new Rectangle();
             if(tile is ForestBox)
-                rectangle.Fill = imageBrushFactory.getImageBrush(EBoxType.FOREST);
+                rectangle.Fill = _imageBrushFactory.getImageBrush(EBoxType.FOREST);
             if(tile is SeaBox)
-                rectangle.Fill = imageBrushFactory.getImageBrush(EBoxType.SEA);
+                rectangle.Fill = _imageBrushFactory.getImageBrush(EBoxType.SEA);
             if(tile is MountainBox)
-                rectangle.Fill = imageBrushFactory.getImageBrush(EBoxType.MOUNTAIN);
+                rectangle.Fill = _imageBrushFactory.getImageBrush(EBoxType.MOUNTAIN);
             if(tile is LowlandBox)
-                rectangle.Fill = imageBrushFactory.getImageBrush(EBoxType.LOWLAND);
+                rectangle.Fill = _imageBrushFactory.getImageBrush(EBoxType.LOWLAND);
             if(tile is DesertBox)
-                rectangle.Fill = imageBrushFactory.getImageBrush(EBoxType.DESERT);
+                rectangle.Fill = _imageBrushFactory.getImageBrush(EBoxType.DESERT);
 
             Grid.SetColumn(rectangle, c);
             Grid.SetRow(rectangle, l);
@@ -197,14 +197,14 @@ namespace IHM {
          * Update informations about the general info panel( players and game info)
          */
         private void updateInfoPanel() {
-            StepLabel.Content = "Tours restants : " + game.getSteps();
-            Units1Label.Content = "Unitées restantes : " + game.getPlayer1().getNation().getUnitsNumber();
-            Units2Label.Content = "Unitées restantes : " + game.getPlayer2().getNation().getUnitsNumber();
-            Points1Label.Content = "Points : " + game.getPlayer1().getPoints();
-            Points2Label.Content = "Points : " + game.getPlayer2().getPoints();
+            StepLabel.Content = "Tours restants : " + _game.getSteps();
+            Units1Label.Content = "Unitées restantes : " + _game.getPlayer1().getNation().getUnitsNumber();
+            Units2Label.Content = "Unitées restantes : " + _game.getPlayer2().getNation().getUnitsNumber();
+            Points1Label.Content = "Points : " + _game.getPlayer1().getPoints();
+            Points2Label.Content = "Points : " + _game.getPlayer2().getPoints();
 
-            var activeLabel = (game.isPlayer1Active()) ? Nation1Label : Nation2Label;
-            var unactiveLabel = (game.isPlayer1Active()) ? Nation2Label : Nation1Label;
+            var activeLabel = (_game.isPlayer1Active()) ? Nation1Label : Nation2Label;
+            var unactiveLabel = (_game.isPlayer1Active()) ? Nation2Label : Nation1Label;
 
             activeLabel.Foreground = Brushes.GreenYellow;
             activeLabel.FontWeight = FontWeights.Bold;
@@ -216,40 +216,40 @@ namespace IHM {
          * Change the color of rectangle where player units are and update the number of units
          */
         private void updateUnitUI() {
-            if (game.getActivePlayer().getNation().getUnits() != null) {
-                List<Unit> selectableUnits1 = game.getActivePlayer().getNation().getUnits().ToList();
+            if (_game.getActivePlayer().getNation().getUnits() != null) {
+                List<Unit> selectableUnits1 = _game.getActivePlayer().getNation().getUnits().ToList();
 
                 foreach (Unit u in selectableUnits1) {
                     Rectangle r = getRectangle(u.getLine(), u.getColumn());
                     r.Stroke = Brushes.GreenYellow;
-                    if (r != selectedVisual)
+                    if (r != _selectedVisual)
                         r.StrokeThickness = 2;
 
                     Ellipse ell = getEllipse(u.getLine(), u.getColumn());
                     ell.Fill = Brushes.GreenYellow;
 
                     TextBlock text = getText(u.getLine(), u.getColumn());
-                    text.Text = "" + game.getActivePlayer().getNation().getUnits(u.getLine(), u.getColumn()).Count;
+                    text.Text = "" + _game.getActivePlayer().getNation().getUnits(u.getLine(), u.getColumn()).Count;
 
                     ell.Visibility = System.Windows.Visibility.Visible;
                     text.Visibility = System.Windows.Visibility.Visible;
                 }
             }
 
-            if (game.getUnactivePlayer().getNation().getUnits() != null) {
-                List<Unit> selectableUnits2 = game.getUnactivePlayer().getNation().getUnits().ToList();
+            if (_game.getUnactivePlayer().getNation().getUnits() != null) {
+                List<Unit> selectableUnits2 = _game.getUnactivePlayer().getNation().getUnits().ToList();
 
                 foreach (Unit u in selectableUnits2) {
                     Rectangle r = getRectangle(u.getLine(), u.getColumn());
                     r.Stroke = Brushes.Red;
-                    if (r != selectedVisual)
+                    if (r != _selectedVisual)
                         r.StrokeThickness = 2;
 
                     Ellipse ell = getEllipse(u.getLine(), u.getColumn());
                     ell.Fill = Brushes.Red;
 
                     TextBlock text = getText(u.getLine(), u.getColumn());
-                    text.Text = "" + game.getUnactivePlayer().getNation().getUnits(u.getLine(), u.getColumn()).Count;
+                    text.Text = "" + _game.getUnactivePlayer().getNation().getUnits(u.getLine(), u.getColumn()).Count;
 
                     ell.Visibility = System.Windows.Visibility.Visible;
                     text.Visibility = System.Windows.Visibility.Visible;
@@ -288,16 +288,14 @@ namespace IHM {
             int row = Grid.GetRow(rectangle);
             int column = Grid.GetColumn(rectangle);
 
-            setDefaultOpacity();
-
-            if(selectedVisual != null) {
-                if(hasUnits(Grid.GetRow(selectedVisual), Grid.GetColumn(selectedVisual)))
-                    selectedVisual.StrokeThickness = 2;
+            if(_selectedVisual != null) {
+                if(hasUnits(Grid.GetRow(_selectedVisual), Grid.GetColumn(_selectedVisual)))
+                    _selectedVisual.StrokeThickness = 2;
                 else
-                    selectedVisual.StrokeThickness = 1;
+                    _selectedVisual.StrokeThickness = 1;
             }
-            selectedVisual = rectangle;
-            selectedVisual.Tag = tile;
+            _selectedVisual = rectangle;
+            _selectedVisual.Tag = tile;
             rectangle.StrokeThickness = 3;
             InfoLabel.Content = String.Format("[{0:00} - {1:00}] {2}", row, column, tile);
 
@@ -306,6 +304,7 @@ namespace IHM {
             }
             updateForStep();
             updateUnitInfo(row, column);
+            setDefaultOpacity();
 
             e.Handled = true;
         }
@@ -318,25 +317,25 @@ namespace IHM {
             int old_row = unit.getLine();
             int old_column = unit.getColumn();
 
-            if (unit.canMove(row, column, map) && (old_row!=row || old_column!=column)) {
+            if (unit.canMove(row, column, _map) && (old_row!=row || old_column!=column)) {
                 //If the opponent has no units, we move
-                if (game.getUnactivePlayer().getNation().getUnits(row, column).Count == 0)
-                    unit.move(row, column, map);
+                if (_game.getUnactivePlayer().getNation().getUnits(row, column).Count == 0)
+                    unit.move(row, column, _map);
                 else {
                     Unit defUnit = null;
                     bool result = true;
 
                     // While there are defensive unit and we win battle we continue to fight
-                    while (result && (defUnit = game.getBestDefensiveUnit(row, column)) != null) {
+                    while (result && (defUnit = _game.getBestDefensiveUnit(row, column)) != null) {
                         result = unit.attack(defUnit);
-                        game.getUnactivePlayer().getNation().deleteDeadUnits();
+                        _game.getUnactivePlayer().getNation().deleteDeadUnits();
                     }
 
                     // If we win battle
                     if (result)
-                        unit.move(row, column, map);
+                        unit.move(row, column, _map);
                     else
-                        game.getActivePlayer().getNation().deleteDeadUnits();
+                        _game.getActivePlayer().getNation().deleteDeadUnits();
 
                 }
             }
@@ -356,8 +355,8 @@ namespace IHM {
          * Update information displayed about unit in the tile with line and column
          */
         private void updateUnitInfo(int line, int column) {
-            List<Unit> unitsActivePlayer = game.getActivePlayer().getNation().getUnits(line, column);
-            List<Unit> unitsUnactivePlayer = game.getUnactivePlayer().getNation().getUnits(line, column);
+            List<Unit> unitsActivePlayer = _game.getActivePlayer().getNation().getUnits(line, column);
+            List<Unit> unitsUnactivePlayer = _game.getUnactivePlayer().getNation().getUnits(line, column);
             List<Unit> nonEmptyList = new List<Unit>();
             StackPanel activePanel = new StackPanel();
 
@@ -365,7 +364,7 @@ namespace IHM {
             unitP2InfoPanel.Children.Clear();
 
             if(unitsActivePlayer.Count > 0) {
-                if (game.isPlayer1Active())
+                if (_game.isPlayer1Active())
                     activePanel = unitP1InfoPanel;
                 else
                     activePanel = unitP2InfoPanel;
@@ -373,7 +372,7 @@ namespace IHM {
             }
 
             if(unitsUnactivePlayer.Count > 0) {
-                if (game.isPlayer1Active())
+                if (_game.isPlayer1Active())
                     activePanel = unitP2InfoPanel;
                 else
                     activePanel = unitP1InfoPanel;
@@ -407,10 +406,10 @@ namespace IHM {
             selectUnit(stack);
 
             if (_selectedUnit != null) {
-                ENation nation = game.getActivePlayer().getNation().nationType;
+                ENation nation = _game.getActivePlayer().getNation().nationType;
                 Unit u = (Unit) _selectedUnit.Tag;
-                int pos = u.getLine() * map.Width + u.getColumn();
-                List<int> moves = wrap.possibleMoves(map.convertMapToIntList(), (int) nation, pos, game.getOpponentUnitsPositions());
+                int pos = u.getLine() * _map.Width + u.getColumn();
+                List<int> moves = _wrap.possibleMoves(_map.convertMapToIntList(), (int) nation, pos, _game.getOpponentUnitsPositions());
 
                 for (int i = 0; i < moves.Count; ) {
                     int row = moves[i++];
@@ -418,7 +417,7 @@ namespace IHM {
 
                     Rectangle r = getRectangle(row, col);
                     //Peut mieux faire (param en plus à passer au cpp pour gérer le cas du 1/2 point de deplacement des gaulois)
-                    if(u.canMove(row, col,map))
+                    if(u.canMove(row, col,_map))
                         r.Opacity = 1;
                 }
             } else
@@ -443,7 +442,7 @@ namespace IHM {
             /*
              * Evite la sélection d'une unité par le joueur adverse lors du changement de tour
              */
-            if (game.getActivePlayer().getNation().getUnits(unit.getLine(), unit.getColumn()).Count > 0) {
+            if (_game.getActivePlayer().getNation().getUnits(unit.getLine(), unit.getColumn()).Count > 0) {
                 if (selectedUnit != _selectedUnit) {
                     Border parent = (Border)selectedUnit.Parent;
                     parent.BorderThickness = new Thickness(3);
@@ -503,31 +502,31 @@ namespace IHM {
 
             Rectangle iLife = new Rectangle();
             iLife.Width = iLife.Height = 24;
-            iLife.Fill = imageBrushFactory.getLifeImage();
+            iLife.Fill = _imageBrushFactory.getLifeImage();
             iLife.ToolTip = "Vie";
 
             Rectangle iPoint = new Rectangle();
             iPoint.Width = iPoint.Height = 24;
-            iPoint.Fill = imageBrushFactory.getPointImage();
+            iPoint.Fill = _imageBrushFactory.getPointImage();
             iPoint.ToolTip = "Point";
 
             Rectangle iSword = new Rectangle();
             iSword.Width = iSword.Height = 24;
-            iSword.Fill = imageBrushFactory.getSwordImage();
+            iSword.Fill = _imageBrushFactory.getSwordImage();
             iSword.ToolTip = "Attaque";
 
             Rectangle iHelmet = new Rectangle();
             iHelmet.Width = iHelmet.Height = 24;
-            iHelmet.Fill = imageBrushFactory.getHelmetImage();
+            iHelmet.Fill = _imageBrushFactory.getHelmetImage();
             iHelmet.ToolTip = "Défense";
 
             Rectangle iBoots = new Rectangle();
             iBoots.Width = iBoots.Height = 24;
-            iBoots.Fill = imageBrushFactory.getBootsImage();
+            iBoots.Fill = _imageBrushFactory.getBootsImage();
             iBoots.ToolTip = "Déplacement";
 
             Label lbPoint = new Label();
-            lbPoint.Content = u.getPoint(game.getMap());
+            lbPoint.Content = u.getPoint(_game.getMap());
             Label lbOff = new Label();
             lbOff.Content = u.getOffensive();
             Label lbDeff = new Label();
@@ -559,21 +558,27 @@ namespace IHM {
          * Check if there still is at least one unit in the game
          */
         private bool hasUnits(int line, int column) {
-            return (game.getActivePlayer().getNation().getUnits(line, column).Count > 0 ||
-                game.getUnactivePlayer().getNation().getUnits(line, column).Count > 0);
+            return (_game.getActivePlayer().getNation().getUnits(line, column).Count > 0 ||
+                _game.getUnactivePlayer().getNation().getUnits(line, column).Count > 0);
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
             InfoLabel.Content = "Pas d'info";
-            if(selectedVisual != null) {
-                if(hasUnits(Grid.GetRow(selectedVisual), Grid.GetColumn(selectedVisual)))
-                    selectedVisual.StrokeThickness = 2;
+            if(_selectedVisual != null) {
+                if(hasUnits(Grid.GetRow(_selectedVisual), Grid.GetColumn(_selectedVisual)))
+                    _selectedVisual.StrokeThickness = 2;
                 else
-                    selectedVisual.StrokeThickness = 1;
+                    _selectedVisual.StrokeThickness = 1;
             }
-            selectedVisual = null;
+            _selectedVisual = null;
+            if (_selectedUnit != null) {
+                foreach (Label lbl in _selectedUnit.Children.OfType<Label>())
+                    lbl.FontWeight = FontWeights.Normal;
+            }
             _selectedUnit = null;
             setDefaultOpacity();
+            unitP1InfoPanel.Children.Clear();
+            unitP2InfoPanel.Children.Clear();
         }
 
         private void setDefaultOpacity() {
